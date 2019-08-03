d3ae30b0b1aaa7fb7c5c5fa461c265cec458e69d
Andres <andres.quintero8@udea.edu.co>
2019-08-03 18:35:30 -0500

    Intento de menu

--- pyth.py
+++ pyth.py
@@ -26,24 +26,32 @@ def readArticle(ar):
 
 
 def searchISSN(value):
+    i = 0
     for article in articles:
         # for issn in article['ISSNs']:
         if article.get('ISSNs') == value:
+            i = i + 1
             # if issn == value:
             print(article.get('Source Title'))
+    if i==0:
+        print('No se encontró ninguna revista asociada al ISSN ', value)
 
 
 def searchName(value):
+    i = 0
     for article in articles:
         if value in article.get('Source Title'):
-            print(article.get('Source Title'))
+            i = i +1
+            print(i, article.get('Source Title'))
+    if i == 0:
+        print('No se encontró ninguna revista con el nombre ', value)
 
 
 # temp = input("Ingrese el campo a buscar")
 # readArticle(temp)
 # temp = input("Ingrese el ISSN")
 # searchISSN(temp)
-temp = input("Ingrese el nombre de la revista")
-searchName(temp)
+#temp = input("Ingrese el nombre de la revista")
+#searchName(temp)
 # readList()
 # readField('Lens ID')
