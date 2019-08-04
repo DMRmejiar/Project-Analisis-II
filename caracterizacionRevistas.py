7e81b226e115783b031e641a702c4a48f974e078
Paola Posada <paola.posada1@udea.edu.co>
2019-08-04 16:12:35 -0500

    Main actualizado

--- caracterizacionRevistas.py
+++ caracterizacionRevistas.py
@@ -28,21 +28,111 @@ class Controller:
         article_object = create_article(article)
         list_articles = [article_object]
         magazine_object = models.Magazine(article.get('Source Title'), article.get('ISSNs'), list_articles)
-        if self.list_magazine.count(magazine_object) > 0:
-            position = self.list_magazine.index(magazine_object)
+        position=self.count(magazine_object)
+        if position != -1:
             self.list_magazine[position].articles.append(article_object)
         else:
             self.list_magazine.append(magazine_object)
 
+    def count(self, magazine):
+        for magazines in self.list_magazine:
+            if magazine.title in magazines.title:
+                return self.list_magazine.index(magazines)
+
+        return -1
+
     def search_name(self, value):
+        i=0
         value = value.lower()
         for article in self.articles:
             if value in str(article.get('Source Title')).lower():
+                i = i + 1
                 self.add_magazine(article)
 
-    def search_issn(self):
-        # Se busca por issn y se llena una lista de revistas con las caracteristicas
-        pass
+    def search_issn(self,issn):
+
+        for article in self.articles:
+            articleIssn = article.get("ISSNs")
+            listissns = articleIssn.split("; ")
+            for issns in listissns:
+                if issn in issns:
+                    self.add_magazine(article)
+
+    def byname(self):
+        while True:
+            print("Ingrese el nombre de la revista o '*' para regresar:")
+            nombre = input()
+            if nombre=='*':
+                break
+            self.list_magazine.clear()
+            self.search_name(nombre)
+            i=0
+            for revista in self.getMagazines():
+                i = i+1
+                print(i, revista.title)
+            if i == 0:
+                print('No se encontró ninguna revista con el nombre "', nombre, '" intete de nuevo')
+                True
+            else:
+                print("Para conocer la informacion de alguna de las revistas anteriores ingrese el "
+                      "numero que le corresponde o '*' para regresar")
+                buscar = input()
+                y = 0
+                try:
+                    for revista in self.getMagazines():
+                        y = y + 1
+                        if y == int(buscar):
+                            print('Abriendo:', '"', revista.title, '"')
+                            False
+                except:
+                    print("Error, solo se aceptan valores numericos")
+                    buscar == '*'
+                if buscar=='*':
+                    break
+            break
+
+
+    def byISSN(self):
+        while True:
+            print("Ingrese el ISSN de la revista o '*' para regresar:")
+            ISSN = input()
+            if ISSN=='*':
+                break
+            self.list_magazine.clear()
+            self.search_issn(ISSN)
+            i=0
+            for revista in self.getMagazines():
+                i = i+1
+                print(i, revista.title)
+            if i == 0:
+                print('No se encontró ninguna revista con el Issn "', ISSN, '" intente de nuevo')
+                True
+            else:
+                print("Para conocer la informacion de alguna de las revistas anteriores ingrese el "
+                      "numero que le corresponde o '*' para regresar")
+                buscar = input()
+                y = 0
+                try:
+                    for revista in self.getMagazines():
+                        y = y + 1
+                        if y == int(buscar):
+                            print('Abriendo:', '"', revista.title, '"')
+                            False
+                except:
+                    print("Error, solo se aceptan valores numericos")
+                    buscar == '*'
+                if buscar == '*':
+                    break
+            break
+
+
+    def exit(self):
+        print("Pulse E para salir, cualquier tecla para volver al menu")
+        salir = input()
+        if salir == 'E' or salir == 'e':
+            exit()
+        else:
+            True
 
     def search(self):
         # Aqui se filtra el tipo de busqueda a realizar, se detecta si es por issn o por nombre de revista
--- main.py
+++ main.py
@@ -3,37 +3,14 @@ import pyth
 from caracterizacionRevistas import *
 
 class Main:
-
-    print("Bienvenido, ¿Qué desea realizar?")
-    print("Opciones\n1.- Buscar revistas por nombre \n2.- Buscar resvistas por ISSN")
-    seleccion = input()
     controlador = Controller()
-
+    print("Bienvenido, ¿Qué desea realizar?")
     while True:
-        if seleccion == '1':
-            print("Ingrese el nombre de la revista:")
-            nombre = input()
-            controlador.search_name(nombre)
-            i=0
-            for revista in controlador.getMagazines():
-                i = i+1
-                print(i, revista.title)
-            if i == 0:
-                print('No se encontró ninguna revista con el nombre "', nombre, '" intete de nuevo')
-                True
-            else:
-                print("Para conocer la informacion de alguna de las revistas anteriores ingrese el "
-                      "numero que le corresponde")
-                buscar = input()
-                break
-
-
-        elif seleccion == '2':
-            print("Ingrese el ISSN de la revista:")
-            ISSN = input()
-            pyth.searchISSN(ISSN)
-            break
-        else:
-            print("Opción invalida, intentelo nuevamente")
-            seleccion = input()
-            True
+        print("Opciones\n1.- Buscar revistas por nombre \n2.- Buscar resvistas por ISSN")
+        seleccion=input()
+        if seleccion == "1":
+            controlador.byname()
+            controlador.exit()
+        if seleccion == "2":
+            controlador.byISSN()
+            controlador.exit()
