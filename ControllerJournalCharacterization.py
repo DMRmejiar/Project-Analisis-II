fde85bbe77fe5e993b44bc00566cc29c678dd2a7
DMRmejiar <dmrmejia57@gmail.com>
2019-10-15 08:45:55 -0500

    Updates ControllerJournalCharacterization

--- ControllerJournalCharacterization.py
+++ ControllerJournalCharacterization.py
@@ -15,6 +15,7 @@ class ControllerJournalCharacterization:
         search_value = str(search_value)
         search_type = str(search_type)
         self.__journal_list = self.__journal_characterization.filter_journals(search_value, search_type)
+        return self.get_journals()
 
     def update_articles_and_volumes(self, journal_index):
         temp_volumes = []
@@ -26,16 +27,10 @@ class ControllerJournalCharacterization:
                 self.__volume_list[article['volume']] = []
         for article in self.__article_list:
             if article['issue'] not in self.__volume_list[article['volume']]:
-                print(str(article['issue']))
-                print('+')
-                self.__volume_list[article['volume']].append(article['issue'])
-
-    def get_articles(self, volume, issue):
-        articles = []
-        for article in self.__article_list:
-            if str(article['volume']) == str(volume) and str(article['issue']) == str(issue):
-                articles.append(article)
-        return articles
+                self.__volume_list[article['volume']] = {article['issue']: [article]}
+            else:
+                self.__volume_list[article['volume']][article['issue']].append(article)
+        return self.get_volumes()
 
     def get_volumes(self):
         return self.__volume_list
